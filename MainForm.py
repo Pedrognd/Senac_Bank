30c4fe48921a6ba39fd20ff6d33467ce7326bbde
Pedrognd <104540763+Pedrognd@users.noreply.github.com>
2022-11-24 14:11:35 -0300

    Update MainForm.py

--- MainForm.py
+++ MainForm.py
@@ -12,7 +12,6 @@ from ast import Break
 import Function as fn
 from User import user
 
-
 class CA:
     def __init__(self,cod,tit,sen,slc,stt):
         self.AccountNumber = cod
@@ -25,6 +24,7 @@ class SA(CA):
     def __init__(self,cod='',tit='',sen=0,slc=0,stt=False):
         super().__init__(cod,tit,sen,slc,stt)
         self.SavingsBalance = 0
+
     # CADASTRO DE CONTA #
     def CAD(self):
         
@@ -66,10 +66,6 @@ class SA(CA):
             except ValueError:
                 print('Valor inválido')
         
-        fn.ajustar()
-        print('Deposito efetuado com sucesso!')
-        print('Conta criada com sucesso')
-        
         # SALVAR CONTA #
         NewUser = {
             "username": UserTIT,
@@ -84,77 +80,94 @@ class SA(CA):
         
     # ACESSAR CONTA CADASTRADA #
     def ACC(self,User_ID='', User_SEN=0):
-        
-        if self.StatusLog == False:
-            fn.cls()
-            fn.ajustar()
-            print('|' + f'{"Acessar Conta":^48}' + '|')
-            fn.ajustar()
-            User_ID = input('Insira o código da sua conta: ')
-            User_SEN = int(input('Insira sua senha: '))
-
-        # VERIFICAÇÃO DE LOGIN #    
-        if User_ID in user and User_SEN == user[User_ID].get('password'):
-            self.StatusLog = True
-
-            self.AccountNumber = User_ID
-
-            self.Username = user[User_ID].get('username')
-            self.Password = user[User_ID].get('password')
-            self.CheckingBalance = user[User_ID].get('checkingBalance')
-            self.SavingsBalance = user[User_ID].get('savingsBalance')
-
-
-        if self.StatusLog == False:
-            print('Usuario nao existe, ou a senha esta incorreta!')
-        else:
-            fn.ajustar()
-            if self.StatusLog == True:
-                pass
-            else:
-                print('Você está logado')
-
-            fn.espera(3)
-            
-            fn.cls()
-
-            def escc():
+        tentativa = 0 
+        while True:   
+            if self.StatusLog == False:
+                fn.cls()
                 fn.ajustar()
-                print('|' + f'{"Selecione uma das Operações":^48}' + '|')
+                print('|' + f'{"Acessar Conta":^48}' + '|')
                 fn.ajustar()
+                User_ID = input('Insira o código da sua conta: ')
+                User_SEN = int(input('Insira sua senha: '))
                 
-                print('|' + f'{"[1] - Mostrar Dados":<48}' + '|')
-                print('|' + f'{"[2] - Depósito":<48}' + '|')
-                print('|' + f'{"[3] - Saque":<48}' + '|')
-                print('|' + f'{"[4] - Aplicação":<48}' + '|')
-                print('|' + f'{"[5] - Resgate":<48}' + '|')
-                print('|' + f'{"[0] - Sair":<48}' + '|')
+            if User_ID in user and User_SEN == user[User_ID].get('password'):
+                self.StatusLog = True
 
-                fn.ajustar()
-            
-                esc = int(input())
-                
-                # CHAMANDO FUNÇÕES #
-                if esc == 1:
-                    aa.MTD()
-                elif esc == 2:
-                    aa.DEP()
-                elif esc == 3:
-                    aa.SAQ()
-                elif esc == 4:
-                    aa.APL()
-                elif esc == 5:
-                    aa.RES()
-                elif esc == 0:
-                    self.StatusLog = False
-                    MainMenu()
-                else:
+                self.AccountNumber = User_ID
+
+                self.Username = user[User_ID].get('username')
+                self.Password = user[User_ID].get('password')
+                self.CheckingBalance = user[User_ID].get('checkingBalance')
+                self.SavingsBalance = user[User_ID].get('savingsBalance')
+                break
+
+
+            if self.StatusLog == False:
+                print('Usuario ou a senha esta incorreta!')
+                tentativa += 1
+                fn.espera(2)
+                if tentativa == 3:
                     fn.cls()
-                    print('Escolha uma das operações')
-                    escc()
-            escc()
+                    fn.ajustar()
+                    print('|' + f'{"Atenção":^48}')
+                    fn.ajustar()
 
-    # FUNÇÃO DE MOSTRAR DADOS #
+                    print('|' + f'{"Numero de tentativas ultrapassada!":^48}' + '|')
+                    print('|' + f'{"A sua conta será bloqueada":^48}' + '|')
+                    print('|' + f'{"Entre em conta com a sua agencia":^48}' + '|')
+                    print('|' + f'{"Fone: 0800 012 9874":^48}' + '|') # Numero ficticio #
+
+                    fn.ajustar()
+                    tentativa = 0
+                    fn.espera(5)
+                    MainMenu()
+
+            else:
+                fn.ajustar()
+                if self.StatusLog == True:
+                    pass
+                else:
+                    print('Você está logado')
+
+                fn.espera(3)
+                
+                fn.cls()
+
+                def escc():
+                    fn.ajustar()
+                    print('|' + f'{"Selecione uma das Operações":^48}' + '|')
+                    fn.ajustar()
+                    
+                    print('|' + f'{"[1] - Mostrar Dados":<48}' + '|')
+                    print('|' + f'{"[2] - Depósito":<48}' + '|')
+                    print('|' + f'{"[3] - Saque":<48}' + '|')
+                    print('|' + f'{"[4] - Aplicação":<48}' + '|')
+                    print('|' + f'{"[5] - Resgate":<48}' + '|')
+                    print('|' + f'{"[0] - Sair":<48}' + '|')
+
+                    fn.ajustar()
+                
+                    esc = int(input())
+
+                    if esc == 1:
+                        aa.MTD()
+                    elif esc == 2:
+                        aa.DEP()
+                    elif esc == 3:
+                        aa.SAQ()
+                    elif esc == 4:
+                        aa.APL()
+                    elif esc == 5:
+                        aa.RES()
+                    elif esc == 0:
+                        self.StatusLog = False
+                        MainMenu()
+                    else:
+                        fn.cls()
+                        print('Escolha uma das operações')
+                        escc()
+                escc()
+
     def MTD(self):
         
             fn.cls()
@@ -172,7 +185,7 @@ class SA(CA):
                 fn.cls()
                 aa.ACC()
             elif test == 2:
-                self.StatusLog = False
+                self.logado = False
                 fn.cls()
                 MainMenu()
             else:
@@ -180,89 +193,58 @@ class SA(CA):
                 print("Operação Invalida")
                 aa.MTD()
 
-    # FUNÇÃO DE DEPOSITO #
     def DEP(self):
         fn.cls()
         fn.ajustar()
         print('|' + f'{"Depóstito":^48}' + '|')
         fn.ajustar()
-        while True:
-            try:
-                vD = float(input('Insira um valor para depósito: R$ '))
-                if vD < 10:
-                    print('Valor minimo de R$ 10,00 !')
+        vD = float(input('Insira um valor para depósito: '))
+        if vD < 10:
+            print('Você não pode realizar o depósito com esse valor!')
 
-                else:
-                    SLCant = self.CheckingBalance
-                    self.CheckingBalance += vD
-                    print('Seu depósito foi realizado com sucesso!')
-                    print('Saldo antes do deposito: R$ ',SLCant)
-                    print('Saldo depois do deposito: R$ ',self.CheckingBalance)
-                fn.espera(3)
-                aa.ACC()
-                break
-            except ValueError:
-                print('Insira um valor valido!')
+        else:
+            self.CheckingBalance += vD
+            print('Seu depósito foi realizado com sucesso!')
+        fn.espera(3)
+        aa.ACC()
 
-    # FUNÇÃO DE SAQUE #
     def SAQ(self):
         fn.cls()
         fn.ajustar()
         print('|' + f'{"Saque":^48}' + '|')
         fn.ajustar()
-        try:
-            print('Valor disponivel para saque: R$ ',self.CheckingBalance)
-            vS = float(input('Insira um valor para o saque: R$ '))
-            if vS > self.CheckingBalance:
-                print('Saldo insuficiente. Seu saldo é R$', self.CheckingBalance)
-            else:
-                SLCant = self.CheckingBalance
-                self.CheckingBalance -= vS
-                print('O saque foi realizado com sucesso!')
-                print('Saldo antes do deposito: R$ ',SLCant)
-                print('Saldo depois do deposito: R$ ',self.CheckingBalance)
-                fn.espera(3)
-                aa.ACC()
-        except AttributeError:  
-            print('Erro')
-            aa.ACC()
-    # FUNÇÃO DE APLICAÇÃO #
+        vS = float(input('Insira um valor para o saque: '))
+        if vS > self.getCheckingBalance():
+            print('Saldo insuficiente. Seu saldo é R$', self.getCheckingBalance())
+        else:
+            saldoAtual = self.getCheckingBalance()
+            saldoAtual -= vS
+            self.setCheckingBalance(saldoAtual)  
+            print('O saque foi realizado com sucesso!')
+
     def APL(self):
         fn.cls()
         fn.ajustar()
         print('|' + f'{"Aplicação Poupança":^48}' + '|')
         fn.ajustar()
-        print('Valor disponivel para aplicação: R$ ',self.CheckingBalance)
-        vA = float(input('Insira um valor para aplicação: R$ '))
+        vA = float(input('Insira um valor para aplicação: '))
         self.CheckingBalance -= vA
         self.SavingsBalance += vA
         print('Sua aplicação foi feita com sucesso!')
-        print('Saldo conta corrente: R$ ',self.CheckingBalance)
-        print('Saldo conta poupança: R$ ',self.SavingsBalance)
         fn.espera(3)
         aa.ACC()
 
-    # FUNÇÃO DE RESGATE #
     def RES(self):
         fn.cls()
         fn.ajustar()
         print('|' + f'{"Resgate Poupança":^48}' + '|')
         fn.ajustar()
-        try:
-            print('Valor disponivel para resgate: R$ ',self.SavingsBalance)
-            vR = float(input('Insira um valor para resgate: R$ '))
-            self.SavingsBalance -= vR
-            self.CheckingBalance += vR
-            print('Seu resgate foi efetuado com sucesso!')
-            print('Saldo conta corrente: R$ ',self.CheckingBalance)
-            print('Saldo conta poupança: R$ ',self.SavingsBalance)
-            fn.espera(3)
-            aa.ACC()
-        except AttributeError:
-            print('Erro')
-            aa.ACC()
+        vR = float(input('Insira um valor para resgate: '))
+        self.SavingsBalanceSLC -= vR
+        print('Seu resgate foi efetuado com sucesso!')
+        fn.espera(3)
+        aa.ACC()
 
-# FUNÇÃO DE "TELA" PRINCIPAL #
 def MainMenu():
     fn.cls()
     try:
